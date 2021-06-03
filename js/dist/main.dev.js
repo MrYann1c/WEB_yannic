480cca54cd7ea6d0dc57905ab1ac67510649408c
Yannic Haas <yannic.haas@quickline.ch>
2021-06-03 11:40:57 +0200

    Rebuilding Photos & Scroll to top

--- js/dist/main.dev.js
+++ js/dist/main.dev.js
@@ -83,4 +83,28 @@ function imgClose() {
   });
 }
 
+var mybutton = document.querySelector('.topBtn'); // When the user scrolls down 20px from the top of the document, show the button
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.visibility = "visible";
+    mybutton.style.opacity = "0.8";
+    mybutton.style.right = "30px";
+  } else {
+    mybutton.style.visibility = "hidden";
+    mybutton.style.opacity = "0";
+    mybutton.style.right = "-30px";
+  }
+} // When the user clicks on the button, scroll to the top of the document
+
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 menuIcon.addEventListener('click', toggleMenuIcon);
